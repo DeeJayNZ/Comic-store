beb2d4d837485d73e5af9e692469b8a36ccd77c0
Dawson <37384968+DeeJayNZ@users.noreply.github.com>
2019-03-11 11:50:31 +1300

    Add files via upload

---
+++ Comic-store-ver-1.3/Comic-store-ver-1.2/ver 1.1.py
@@ -0,0 +1,38 @@
+
+from bottle import run, route, view, get, post, request
+from itertools import count
+
+class comic_book:
+    _ids = count(0)
+    
+    def __init__(self, name, image, stock):
+        self.id = next(self._ids)
+        self.comic_name = name
+        self.comic_image = image
+        self.comic_stock = stock
+    
+    
+comic_test = [
+    comic_book("Super Dude", "image", 8),
+    comic_book("Lizard Man", "image", 12),
+    comic_book("Water Woman", "image", 3)
+    ]
+
+
+
+
+
+
+@route("/")
+@view("index")
+def index(): #this function attatches decorators above
+    pass
+
+
+@route("/comics")
+@view("comics")
+def check_in():
+    pass
+
+run(host="0.0.0.0", port = 8080, reloader=True, debug=True)
+
